1e82b78f14bb8f40ec2c5d0a2bfb04cb4434a9a9
Volodymyr <babihuk888@gmail.com>
2024-11-20 13:58:25 +0200

    fix App

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,18 @@
 import { Route, BrowserRouter as Router, Routes } from 'react-router-dom';
 import EmployeeProfile from './features/EmployeeProfile/index';
+import ErrorPage from './pages/ErrorPage';
 import HomePage from './pages/HomePage/index';
 
-const App = () => {
-  return (
-    <Router>
-      <div className="bg-gray-200 min-h-screen">
-        <Routes>
-          <Route path="/" element={<HomePage />} />
-          <Route path="/employees/:id" element={<EmployeeProfile />} />
-        </Routes>
-      </div>
-    </Router>
-  );
-};
+const App = () => (
+  <Router>
+    <div className="bg-gray-200 min-h-screen">
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/employees/:id" element={<EmployeeProfile />} />
+        <Route path="*" element={<ErrorPage message="404 - Page Not Found" />} />
+      </Routes>
+    </div>
+  </Router>
+);
 
 export default App;
